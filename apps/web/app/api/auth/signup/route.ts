474a3cfe889f84b78fbcf1c3e07c4f0e5d71b3f9
mukeshdidel <mukeshdidel11111@gmail.com>
2025-09-07 20:30:05 +0530

    debugging deployment

--- apps/web/app/api/auth/signup/route.ts
+++ apps/web/app/api/auth/signup/route.ts
@@ -7,26 +7,35 @@ import jwt from "jsonwebtoken";
 export async function POST(req: NextRequest) {
     try {
         const body = await req.json();
+        console.log(body);
 
         const result = signupSchema.safeParse(body);
         if (!result.success) {
             return NextResponse.json({ error: "invalid inputs" }, { status: 400 });
         }
 
+        console.log(1);
+        
         const { name, email, password, userType } = result.data;
 
         const existingDoc = await prisma.doctor.findUnique({ where: { email } });
         const existingPatient = await prisma.patient.findUnique({ where: { email } });        
 
+        console.log(2);
+        
         if (existingDoc || existingPatient) {
             return NextResponse.json({ error: "User already exists" }, { status: 400 });
         }
 
+        console.log(3);
+        
         
         // Create new user
         const hassedPassword = await bcrypt.hash(password, 10);
 
    
+        console.log(4);
+        
         if(userType === "doctor"){
             const newUser = await prisma.doctor.create({
                 data: {
@@ -36,12 +45,17 @@ export async function POST(req: NextRequest) {
                 }
             })
 
+            console.log(5.1);
+            
             const token = jwt.sign({
                 email: newUser.email,
                 id: newUser.id,
                 userType: "doctor",
                 name: newUser.name
             }, process.env.JWT_SECRET!)
+
+            console.log(6.2);
+            
 
             return NextResponse.json({
                 message: "User created successfully",
@@ -58,12 +72,17 @@ export async function POST(req: NextRequest) {
                 }
             })
 
+            console.log(5.2);
+            
+
             const token = jwt.sign({
                 email: newUser.email,
                 id: newUser.id,
                 userType: "patient",
                 name: newUser.name
             }, process.env.JWT_SECRET!)
+            
+            console.log(6.2);
             
             return NextResponse.json({
                 message: "User created successfully",

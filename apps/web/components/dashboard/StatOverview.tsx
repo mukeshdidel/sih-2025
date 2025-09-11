7d334af5fa8a2e48598da81e37f6907df432a57e
mukeshdidel <mukeshdidel11111@gmail.com>
2025-09-11 11:20:46 +0530

    just some color changes

--- apps/web/components/dashboard/StatOverview.tsx
+++ apps/web/components/dashboard/StatOverview.tsx
@@ -5,8 +5,8 @@ const StatOverview = ({ text, number, Icon}: { text: string; number: number; Ico
     <div className="bg-gray-700 rounded-2xl shadow-lg p-6 border border-green-100">
         <div className="flex items-center justify-between text-white">
             <div>
-            <p className="text-sm font-medium text-white">{text}</p>
-            <p className="text-3xl font-bold text-gray-400">{number}</p>
+            <p className="text-lg font-bold text-white">{text}</p>
+            <p className="text-3xl font-bold text-green-400">{number}</p>
             </div>
             {Icon}
         </div>
